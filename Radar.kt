9e4046ef14501038d061c9a49f6f319ef30ee7fc
t4b0t <abdoumerah41@gmail.com>
2025-07-23 08:48:29 +0100

    Create Radar.kt

---
+++ Radar.kt
@@ -0,0 +1,12 @@
+package dz.ultragov.securex
+
+import android.content.Context
+import android.util.Log
+
+object Radar {
+
+    fun startRadar(context: Context) {
+        // Placeholder للكود الفعلي
+        Log.d("UltraGovSecureX", "Radar activated")
+    }
+}
