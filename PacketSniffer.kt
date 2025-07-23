66d7d08b94ffe225ec94cb5125b7a65dd0c032d3
t4b0t <abdoumerah41@gmail.com>
2025-07-23 08:45:35 +0100

    Create PacketSniffer.kt

---
+++ PacketSniffer.kt
@@ -0,0 +1,12 @@
+package dz.ultragov.securex
+
+import android.content.Context
+import android.util.Log
+
+object PacketSniffer {
+
+    fun startSniffing(context: Context) {
+        // Placeholder للكود الفعلي
+        Log.d("UltraGovSecureX", "Packet Sniffer started")
+    }
+}
