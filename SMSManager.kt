ec76ccaeb5ec6d47bd57b1b5b4cc3124715fd070
t4b0t <abdoumerah41@gmail.com>
2025-07-23 08:47:20 +0100

    Create SMSManager.kt

---
+++ SMSManager.kt
@@ -0,0 +1,12 @@
+package dz.ultragov.securex
+
+import android.content.Context
+import android.util.Log
+
+object SMSManager {
+
+    fun launch(context: Context) {
+        // Placeholder للكود الفعلي
+        Log.d("UltraGovSecureX", "SMS Manager active")
+    }
+}
