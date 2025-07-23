329d99be04ef2081b3f05eead50f68ffbc0f2ac4
t4b0t <abdoumerah41@gmail.com>
2025-07-23 08:46:32 +0100

    Create NetworkScanner.kt

---
+++ NetworkScanner.kt
@@ -0,0 +1,12 @@
+package dz.ultragov.securex
+
+import android.content.Context
+import android.util.Log
+
+object NetworkScanner {
+
+    fun scanNetworks(context: Context) {
+        // Placeholder للكود الفعلي
+        Log.d("UltraGovSecureX", "Network Scanner running")
+    }
+}
